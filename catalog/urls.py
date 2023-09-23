d7fce2180a77d4f3a7ec086fd84d4b2e0eaab90d
exetch <gassemark@gmail.com>
2023-09-23 16:27:16 +0300

    feedback implemented

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('contacts/', views.contact_view, name='contact'),
+    path('contacts/', views.contact_view, name='contacts'),
+    path('success/', views.contact_submit_view, name='contact_submit'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 def home_view(request):
     # отображение домашней страницы
     return render(request, 'catalog/home.html')
@@ -7,3 +6,16 @@ def home_view(request):
 def contact_view(request):
     # отображение страницы с контактной информацией
     return render(request, 'catalog/contact.html')
+
+def contact_submit_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        phone = request.POST.get('phone', '')
+        message = request.POST.get('message', '')
+        # Данные из формы получены
+        print(f"Имя: {name}")
+        print(f"Телефон: {phone}")
+        print(f"Сообщение: {message}")
+        return render(request, 'catalog/success.html')
+
+    return render(request, 'contacts.html')
