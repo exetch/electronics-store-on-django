7ce762ddf79efac08814227a6c91ff7f9c3a77ad
exetch <gassemark@gmail.com>
2023-10-26 16:56:54 +0300

    .env updated

--- config/settings.py
+++ config/settings.py
@@ -158,11 +158,11 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 
-CACHE_ENABLED=True
+CACHE_ENABLED = os.getenv("CACHE_ENABLED") == 'True'
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": os.getenv("CACHE_LOCATION"),
     }
 }
 
